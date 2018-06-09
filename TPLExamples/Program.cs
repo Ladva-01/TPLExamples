f4280bb799d742201931c578825213bb752405b0
ladva <hitesh.ladva@realsoft.co.uk>
2018-06-09 16:14:33 +0100

    Add project files.

---
+++ TPLExamples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TPLExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            var retry = new Retry();
+            var someValue = "";
+            var cancellationToken = new System.Threading.CancellationToken();
+            var task = retry.HandleException<OutOfMemoryException>()
+                 .SuccessChecker(() =>
+                     {
+                         Console.WriteLine("Checking...");
+                         return someValue != "Hello";
+                     }
+                 )
+                 .AttemptsExhausted(() => Console.WriteLine("Failed"))
+                 .CancellationToken(cancellationToken)
+                 .Wait(1)
+                 .RunAsync(() =>
+                 {
+                     someValue = Run(1, "");
+                 }
+             , 10);
+
+            task.Wait();
+            Console.ReadLine();
+        }
+
+        public static string Run(int a, string b)
+        {
+            //throw new Exception();
+            return "Hello";
+        }
+    }
+}
---
+++ TPLExamples/Retry.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPLExamples
+{
+    public interface IRetry
+    {
+        IRetry HandleException<TException>() where TException : Exception;
+        IRetry SuccessChecker(Func<bool> successChecker);
+        IRetry AttemptsExhausted(Action attemptsExhuasted);
+        IRetry CancellationToken(CancellationToken cancellationToken);
+        IRetry Wait(int sec);
+        void Run(Action action, int tryCount);
+        Task RunAsync(Action action, int tryCount);
+    }
+
+    public class Retry : IRetry
+    {
+        readonly List<Type> _exceptions;
+
+        Func<bool> _successChecker;
+        Action _attemptsExhuasted;
+        CancellationToken _cancellationToken;
+        int _wait;
+
+        public Retry()
+        {
+            _cancellationToken = new CancellationToken();
+            _exceptions = new List<Type>
+            {
+                typeof(Exception)
+            };
+            _successChecker = () => { return true; };
+            _attemptsExhuasted = () => { };
+        }
+
+        public IRetry HandleException<TException>() where TException : Exception
+        {
+            _exceptions.Remove(typeof(Exception));
+            _exceptions.Add(typeof(TException));
+            return this;
+        }
+
+        public IRetry SuccessChecker(Func<bool> successChecker)
+        {
+            _successChecker = successChecker;
+            return this;
+        }
+
+        public void Run(Action action, int tryCount)
+        {
+            if (tryCount < 1) tryCount = 1;
+
+            int attempt = 0;
+
+            while (tryCount > attempt && !_cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    action();
+
+                    if (_successChecker()) { break; }
+
+                    attempt++;
+                    Thread.Sleep(_wait * 1000);
+                }
+                catch (Exception ex)
+                {
+                    if (!_HandleExcption(ex.GetType()))
+                    {
+                        throw;
+                    }
+                    attempt++;
+                    Thread.Sleep(_wait * 1000);
+                }
+            }
+            _attemptsExhuasted();
+        }
+
+        public IRetry Wait(int sec)
+        {
+            _wait = sec;
+            return this;
+        }
+
+        bool _HandleExcption(Type ex)
+        {
+            return _exceptions.Any(x => x.IsAssignableFrom(ex));
+        }
+
+        public IRetry AttemptsExhausted(Action attemptsExhuasted)
+        {
+            _attemptsExhuasted = attemptsExhuasted;
+            return this;
+        }
+
+        public IRetry CancellationToken(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+            return this;
+        }
+
+        Task _Run(Action action, int tryCount)
+        {
+            var task =  new Task(() => { Run(action, tryCount); });
+            task.Start();
+            return task;
+        }
+
+        public async Task RunAsync(Action action, int tryCount)
+        {
+            await _Run(action, tryCount);
+        }
+    }
+}
